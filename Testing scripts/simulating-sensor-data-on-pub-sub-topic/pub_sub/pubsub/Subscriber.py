58da416cf4733afaa0aeede06b50c3c2082ca071
azra-asiksoy-hydro <azra.asiksoy@hydroteam.be>
2025-02-27 01:02:55 +0200

    implemented the functions of the Subscriber.py file

--- Testing scripts/simulating-sensor-data-on-pub-sub-topic/pub_sub/pubsub/Subscriber.py
+++ Testing scripts/simulating-sensor-data-on-pub-sub-topic/pub_sub/pubsub/Subscriber.py
@@ -9,6 +9,8 @@ class Subscriber:
             project_id (str): project ID.
             subscription_name (str): The name of the subscription to listen to.
         """
+        self.subscriber = pubsub_v1.SubscriberClient()
+        self.subscription_path = self.subscriber.subscription_path(project_id, subscription_name)
         pass
 
     def subscribe(self, callback: callable):
@@ -16,6 +18,9 @@ class Subscriber:
         arguments:
             callback (callable): A function to process incoming messages.
         """
+        streaming_pull_future = self.subscriber.subscribe(self.subscription_path, callback=callback)
+        print(f"Listening for messages on {self.subscription_path}...")
+        streaming_pull_future.result()
         pass
 
     def acknowledge(self, message):
@@ -23,6 +28,8 @@ class Subscriber:
         Acknowledge a message after it has been processed successfully.
         arguments:  message: The Pub/Sub message to acknowledge.
         """
+        self.subscriber.acknowledge(subscription=self.subscription_path, ack_ids=[message.ack_id])
+        print(f"Acknowledged message: {message.message_id}")
         pass
 
     def pull_messages(self, max_messages: int) -> list:
@@ -31,10 +38,16 @@ class Subscriber:
         arguments: max_messages (int): The maximum number of messages to pull.
         Returns: A list of messages.
         """
+        response = self.subscriber.pull(request={"subscription": self.subscription_path, "max_messages": max_messages})
+        messages = response.received_messages
+        print(f"Pulled {len(messages)} messages.")
+        return messages
         pass
 
     def close(self):
         """
         Close the subscriber client 
         """
+        self.subscriber.close()
+        print("Subscriber client closed.")
         pass
