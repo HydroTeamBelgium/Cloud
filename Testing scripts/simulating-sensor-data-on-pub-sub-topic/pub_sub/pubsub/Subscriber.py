c9f4425420972c43cd6b7128b9957996278f92bd
azra-asiksoy-hydro <azra.asiksoy@hydroteam.be>
2025-03-06 23:18:41 +0100

    Added new methods for deserializing and calculating the message size

--- Testing scripts/simulating-sensor-data-on-pub-sub-topic/pub_sub/pubsub/Subscriber.py
+++ Testing scripts/simulating-sensor-data-on-pub-sub-topic/pub_sub/pubsub/Subscriber.py
@@ -5,12 +5,13 @@ class Subscriber:
     A class to handle subscribing to a Pub/Sub topic and processing messages.
     """
 
-    def __init__(self, project_id: str, subscription_name: str):
+    def __init__(self, project_id: str, subscription_name: str, protobuf_class):
         """
         Initialize the Subscriber.
         Args:
             project_id (str): The Google Cloud project ID.
             subscription_name (str): The name of the subscription to listen to.
+            protobuf_class : The Protobuf message class
         """
 
         self.subscriber = pubsub_v1.SubscriberClient()
@@ -32,7 +33,7 @@ class Subscriber:
         """
         Acknowledge a message after it has been processed.
         Args: 
-            message: The Pub/Sub message to be acknowledged.
+            message: The message to be acknowledged.
         """
         self.subscriber.acknowledge(subscription=self.subscription_path, ack_ids=[message.ack_id])
         print(f"Acknowledged message is: {message.message_id}")
@@ -57,3 +58,23 @@ class Subscriber:
         self.subscriber.close()
         
         print("Subscriber client closed.")
+
+    def deserialize_protobuf(self, binary_data):
+        """
+        Deserialize binary data into a Protobuf object.
+        Args: 
+            binary_data (bytes): The binary data to deserialize.
+        Returns:
+            The deserialized Protobuf object.
+        """
+        protobuf_message = self.protobuf_class()
+        protobuf_message.ParseFromString(binary_data)
+        return protobuf_message
+    
+    def message_size(self, messages):
+        """"
+        Calculate the size of the binary message"
+        Args : 
+            messages : Messages pulled from the subscription
+        """ 
+        print(f"Pulled {len(messages)} messages.")
