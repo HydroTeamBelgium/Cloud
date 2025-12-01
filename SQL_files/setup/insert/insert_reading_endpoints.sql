60652114a031dad775582cf67eba8a3a7eea7098
Luigi Fanuzzi <luigi.fanuzzi@hydroteam.be>
2025-12-01 11:14:16 +0100

    removed renamed sql files

--- SQL_files/setup/insert/insert_reading_endpoints.sql
+++
@@ -1,7 +0,0 @@
-INSERT IGNORE INTO reading_end_point (
-    id,
-    name,
-    car_component,
-    description
-)
-VALUES (%s, %s, %s, %s);
--- SQL_files/setup/insert/insert_sensor_typer_measurement_type.sql
+++
@@ -1,5 +0,0 @@
-INSERT IGNORE INTO sensor_type_measurement_type (
-    sensor_type_id,
-    measurement_type_id
-)
-VALUES (%s, %s);
